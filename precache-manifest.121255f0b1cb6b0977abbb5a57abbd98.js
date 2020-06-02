73b723ea44ef547cb9361287fdf3d2dcacac6119
IrynaZolkina <wzyz7557@hotmail.com>
2020-06-01 19:03:10 -0700

    Updates

---
+++ precache-manifest.121255f0b1cb6b0977abbb5a57abbd98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fdc12f414fab1620b4ae5edcb5079db",
+    "url": "/e-commerce-shop/index.html"
+  },
+  {
+    "revision": "b3701c1068edff65cf41",
+    "url": "/e-commerce-shop/static/css/main.581ee52a.chunk.css"
+  },
+  {
+    "revision": "8df04d8ceb2b815ae1c8",
+    "url": "/e-commerce-shop/static/js/2.22abef0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/e-commerce-shop/static/js/2.22abef0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3701c1068edff65cf41",
+    "url": "/e-commerce-shop/static/js/main.1ff335e4.chunk.js"
+  },
+  {
+    "revision": "caa70c304c3265707577",
+    "url": "/e-commerce-shop/static/js/runtime-main.880f9ba0.js"
+  }
+]);
