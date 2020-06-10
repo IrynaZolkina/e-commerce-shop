e5be9c7336268a140e0143f41ec682297dbb6b07
IrynaZolkina <wzyz7557@hotmail.com>
2020-06-10 10:19:29 -0700

    75%

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+//import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
@@ -11,11 +12,11 @@ import { store, persistor } from "./redux/store";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <Router>
         <PersistGate persistor={persistor}>
           <App />
         </PersistGate>
-      </BrowserRouter>
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
